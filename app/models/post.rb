a94030a31fd33be500189b0e0244000fa4fec838
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-05 22:06:27 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class MyValidator < ActiveModel::Validator
   def validate(record)
-    unless record.name.include? ["Won't Believe", "Secret", "Top [number]"] 
+    unless record.name.include? ["Won't Believe", "Secret", "Top [number]", "Guess"] 
       record.errors[:name] << 'Need a name starting with X please!'
     end
   end
