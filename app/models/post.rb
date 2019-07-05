f595b79001578faae14a59e525af70196b867b29
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-05 22:07:26 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,8 @@ end
 
 
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  
   validates :title, presence: true
   validates(:content, { :length => { :minimum => 250 } })
   validates(:summary, { :length => { :maximum => 250 } })
