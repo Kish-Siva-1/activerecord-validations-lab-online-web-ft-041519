0e50a0a4b1beea7d21d69e79db2b8111d7714e7b
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-05 22:07:33 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,10 +9,11 @@ end
 
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-  
+
   validates :title, presence: true
   validates(:content, { :length => { :minimum => 250 } })
   validates(:summary, { :length => { :maximum => 250 } })
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates_with MyValidator
 
 end
