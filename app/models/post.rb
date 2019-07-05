86e7d454de0f66bf02c6570a9911f1325b0bdf8f
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-05 21:56:52 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates(:short_content, { :length => { :minimum => 250 } })
-
+  validates(:content, { :length => { :minimum => 250 } })
+  
 end
