1ad4493fa9aa0f9d7de1c6a3f6253e30669f8738
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-05 22:05:48 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
-
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.name.include? ['X', 
+      record.errors[:name] << 'Need a name starting with X please!'
+    end
+  end
+end
 
 
 class Post < ActiveRecord::Base
